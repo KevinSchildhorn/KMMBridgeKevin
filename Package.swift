9fb2da0941c9a23be025523eeb8203ed6c323a9c
Anka <runner@Mac-1734111086948.local>
2024-12-13 17:58:01 +0000

    KMP SPM package release for 0.2.3-ce92eb7

--- Package.swift
+++ Package.swift
@@ -5,11 +5,15 @@ let packageName = "KevinKMM"
 
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'analytics' (do not edit)
-let AnalyticsPackageName = "analytics"
+let remoteanalyticsUrl = "https://api.github.com/repos/KevinSchildhorn/KMMBridgeKevin/releases/assets/213173195.zip"
+let remoteanalyticsChecksum = "f8fcd942f71fa874e04ed0ac19fce26ecfe6b452cc85821461927557c4de1e9d"
+let analyticsPackageName = "analytics"
 // END KMMBRIDGE BLOCK FOR 'analytics'
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'breeds' (do not edit)
-let BreedsPackageName = "breeds"
+let remotebreedsUrl = "https://api.github.com/repos/KevinSchildhorn/KMMBridgeKevin/releases/assets/213174399.zip"
+let remotebreedsChecksum = "5b648db0f63faeb5c5005fc386a6191d7a287de0a23dfae3dd996d02972eec88"
+let breedsPackageName = "breeds"
 // END KMMBRIDGE BLOCK FOR 'breeds'
 
 
