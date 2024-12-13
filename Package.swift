4930ad5556a723bbf285cbb2d1817895072f200b
Anka <runner@Mac-1734106916954.local>
2024-12-13 16:40:07 +0000

    KMP SPM package release for 0.2.3-5e77da4

--- Package.swift
+++ Package.swift
@@ -5,15 +5,15 @@ let packageName = "KevinKMM"
 
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'analytics' (do not edit)
-let remoteAnalyticsUrl = "https://sso-sdks.cdotools.io/IdentitySDKCore-2.7.0-headless-2-12888440-1155-44dc-9c30-47d7b627ee72.xcframework.zip"
-let remoteAnalyticsChecksum = "675aa4d16cab1b8704d3c3f36f934392b30b0cac4030d559fc15bb4968701198"
-let AnalyticsPackageName = "analytics"
+let remoteanalyticsUrl = "https://api.github.com/repos/KevinSchildhorn/KMMBridgeKevin/releases/assets/213162140.zip"
+let remoteanalyticsChecksum = "7b45c63e4ba34f3829c8529e3c31af0696e4afaddecdfe5ac14297a199b3a9ac"
+let analyticsPackageName = "analytics"
 // END KMMBRIDGE BLOCK FOR 'analytics'
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'breeds' (do not edit)
-let remoteBreedsUrl = "https://sso-sdks.cdotools.io/IdentitySDKHeadless-2.7.0-headless-2-8d7dc73f-ec01-42f5-bd00-196e70446714.xcframework.zip"
-let remoteBreedsChecksum = "63de919d1c98d6aefdf85abf046e8d8736ca134b49d1cb6f65a0b38d4cd799e5"
-let BreedsPackageName = "breeds"
+let remotebreedsUrl = "https://api.github.com/repos/KevinSchildhorn/KMMBridgeKevin/releases/assets/213163189.zip"
+let remotebreedsChecksum = "a569d5eff13600cf19711f9a4801213abfa08b63606c3db2ccfabb484184a7c0"
+let breedsPackageName = "breeds"
 // END KMMBRIDGE BLOCK FOR 'breeds'
 
 
