6f57e054c859479e34aa6c0548dfdd8eb353ec34
Anka <runner@Mac-1734448628191.local>
2024-12-17 15:51:28 +0000

    KMP SPM package release for 0.2.8

--- Package.swift
+++ Package.swift
@@ -5,14 +5,14 @@ let packageName = "KevinKMM"
 
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'analytics' (do not edit)
-let remoteanalyticsUrl = "https://api.github.com/repos/KevinSchildhorn/KMMBridgeKevin/releases/assets/213759460.zip"
-let remoteanalyticsChecksum = "4a72e61312ec0f452b3bc1d9ec2ab3802f84c720a88a41d9c886fbdc3449ce51"
+let remoteanalyticsUrl = "https://api.github.com/repos/KevinSchildhorn/KMMBridgeKevin/releases/assets/213973763.zip"
+let remoteanalyticsChecksum = "4465972c1b56382128a38a41f27d19304b3b98429d7553c6eb751aba8907db43"
 let analyticsPackageName = "analytics"
 // END KMMBRIDGE BLOCK FOR 'analytics'
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'breeds' (do not edit)
-let remotebreedsUrl = "https://api.github.com/repos/KevinSchildhorn/KMMBridgeKevin/releases/assets/213759628.zip"
-let remotebreedsChecksum = "5765d4edcaad720f06a8b2de24fe9777ed3da70de16e6c30a21b01be67e0233e"
+let remotebreedsUrl = "https://api.github.com/repos/KevinSchildhorn/KMMBridgeKevin/releases/assets/213976529.zip"
+let remotebreedsChecksum = "80d1af2904e13f7cea002b6836ac78c5b2ed14638a3c8905718ecd00f619d55e"
 let breedsPackageName = "breeds"
 // END KMMBRIDGE BLOCK FOR 'breeds'
 
