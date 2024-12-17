dfe0a41ebfe900db4e9e40d94bf4fcadf8b4900c
Anka <runner@Mac-1734452870796.local>
2024-12-17 16:56:49 +0000

    KMP SPM package release for 0.2.9

--- Package.swift
+++ Package.swift
@@ -1,55 +1,29 @@
 // swift-tools-version:5.8
 import PackageDescription
 
-let packageName = "KevinKMM"
-
-
-// BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'analytics' (do not edit)
-let remoteanalyticsUrl = "https://api.github.com/repos/KevinSchildhorn/KMMBridgeKevin/releases/assets/213973763.zip"
-let remoteanalyticsChecksum = "4a72e61312ec0f452b3bc1d9ec2ab3802f84c720a88a41d9c886fbdc3449ce51"
-let analyticsPackageName = "analytics"
-// END KMMBRIDGE BLOCK FOR 'analytics'
-
-// BEGIN KMMBRIDGE VARIABLES BLOCK FOR 'breeds' (do not edit)
-let remotebreedsUrl = "https://api.github.com/repos/KevinSchildhorn/KMMBridgeKevin/releases/assets/213976529.zip"
-let remotebreedsChecksum = "5765d4edcaad720f06a8b2de24fe9777ed3da70de16e6c30a21b01be67e0233e"
-let breedsPackageName = "breeds"
-// END KMMBRIDGE BLOCK FOR 'breeds'
-
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://api.github.com/repos/KevinSchildhorn/KMMBridgeKevin/releases/assets/213990059.zip"
+let remoteKotlinChecksum = "cdfe5252f755122fe351945c214f919cd96817fd532bf9a7c183c5f96d42bb30"
+let packageName = "breeds"
+// END KMMBRIDGE BLOCK
 
 let package = Package(
     name: packageName,
     platforms: [
-        .iOS(.v15)
+        .iOS(.v14)
     ],
     products: [
         .library(
             name: packageName,
-            type: .static,
-            targets: [analyticsPackageName, breedsPackageName]
-        ),
-        .library(
-            name: analyticsPackageName,
-            type: .static,
-            targets: [analyticsPackageName]
-        ),
-        .library(
-            name: breedsPackageName,
-            type: .static,
-            targets: [breedsPackageName]
+            targets: [packageName]
         ),
     ],
     targets: [
         .binaryTarget(
-            name: analyticsPackageName,
-            path: remoteanalyticsUrl,
-            checksum: remoteanalyticsChecksum
-
-        ),
-        .binaryTarget(
-            name: analyticsPackageName,
-            path: remotebreedsUrl,
-            checksum: remotebreedsChecksum
-        ),
+            name: packageName,
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
+        )
+        ,
     ]
 )
